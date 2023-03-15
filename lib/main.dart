3cd757d3f0a5fe0610ef9998f96385f531462c74
JoseVinicius-P <josevpf123@gmail.com>
2023-03-14 22:18:46 -0300

    refactor: organizing code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/blocs/MyAppState.dart';
+import 'package:my_app/pages/FavoritesPage.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -163,34 +164,6 @@ class BigCard extends StatelessWidget {
             semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
-    );
-  }
-}
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('Você não tem nenhum favorito'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('Você tem '
-              '${appState.favorites.length} favoritos:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
     );
   }
 }
