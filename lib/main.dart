a94f663c139ada3d36db178d2ba97f6af5a9ba98
JoseVinicius-P <josevpf123@gmail.com>
2023-03-14 22:12:39 -0300

    refactor: organizing code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/utilities/MyAppState.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -23,26 +24,6 @@ class MyApp extends StatelessWidget {
         home: MyHomePage(),
       ),
     );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext(){
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-
-  void toggleFavorite(){
-    if(favorites.contains(current)){
-      favorites.remove(current);
-    }else{
-      favorites.add(current);
-    }
-    notifyListeners();
   }
 }
 
